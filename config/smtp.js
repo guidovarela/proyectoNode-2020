88ef694fa253bd3ac715df00b66c713739926265
Guido Varela Coletta <guido.varela@gmail.com>
2020-12-21 18:27:41 -0300

    Update smtp.js

--- config/smtp.js
+++ config/smtp.js
@@ -5,8 +5,8 @@ let transporter = nodemailer.createTransport({
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: "guido.varela@gmail.com", // user del correo
-      pass: "lsiewybmcyfnpxee", // password del correo -> si es Gmail, usar una contraseña de aplicacion
+      user: "user", // user del correo
+      pass: "pass", // password del correo -> si es Gmail, usar una contraseña de aplicacion
     },
   });
 
